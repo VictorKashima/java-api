d6e04ee36477653b5eb23b6d6b82feac9268d6ad
victorkashima <victorkashima04@gmail.com>
2023-07-11 20:28:10 -0300

    CHORE: Remove @Repository annotation
    Cause, don't need at 3.0+ Spring Boot Version

--- portfolio/src/main/java/com/br/api/portfolio/repository/DesignProjectRepository.java
+++ portfolio/src/main/java/com/br/api/portfolio/repository/DesignProjectRepository.java
@@ -1,7 +1,6 @@
 package com.br.api.portfolio.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.br.api.portfolio.model.DesignProject;
 
